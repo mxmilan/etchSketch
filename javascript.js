3c47924655fd09216fd3d816e147ab789e9546e1
Mxmilan <121529157+mxmilan@users.noreply.github.com>
2024-08-24 19:43:52 -0400

    changed grid to change based on input to function

--- javascript.js
+++ javascript.js
@@ -1,17 +1,18 @@
 const container = document.querySelector(".container");
 
-let  divs = 0;
-while (divs < 256) {
-    let box = document.createElement("div");
-    box.classList.add('grid-item');
-    container.appendChild(box);
+function getGrid (size) {
+    container.innerHTML = '';
+    let itemSize = 800/size; 
+    for (let i = 0; i < size * size; i++) {
+        let box = document.createElement("div");
+        box.classList.add('grid-item');
+        box.style.width = `${itemSize}px`;
+        box.style.height = `${itemSize}`;
+        container.appendChild(box);
 
-    const changeColor = () => {
-        box.style.backgroundColor = "white";
-    };
-
-    box.addEventListener('mouseover', changeColor);
-    box.addEventListener('mouseenter', changeColor);
-
-    divs ++;
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = "white";
+        });
+    }
 };
+getGrid(16);
