1b15b4a9b30543d6221dca398cee563f7688394c
Mxmilan <121529157+mxmilan@users.noreply.github.com>
2024-08-24 21:14:07 -0400

    changes color when button clicked

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container");
 
-function getGrid (size) {
-    container.innerHTML = '';
-    let itemSize = 800/size; 
+function getGrid(size) {
+    container.innerHTML = ''; // Clear the container
+    let itemSize = 800 / size; 
     for (let i = 0; i < size * size; i++) {
         let box = document.createElement("div");
         box.classList.add('grid-item');
@@ -10,24 +10,44 @@ function getGrid (size) {
         box.style.height = `${itemSize}px`;
         container.appendChild(box);
 
+        // Change color to white on hover
         box.addEventListener('mouseover', () => {
             box.style.backgroundColor = "white";
         });
     }
-};
+}
 
-const buttons = document.querySelector(".buttons");
 const fstButton = document.querySelector(".firstButton");
 fstButton.addEventListener('click', () => {
-    let size = prompt('');
-    if (size <= 100) {
+    let size = prompt('Enter grid size (1-100):');
+    size = parseInt(size, 10); // Convert the input to an integer
+    if (size > 0 && size <= 100) {
         getGrid(size);
-    } else if (size > 100) {
-       alert ("pick a different number between 0-100");
     } else {
-        alert ("pick a number between 0-100");
+        alert("Please pick a number between 1 and 100.");
     }
 });
+
+function ranColor() {
+    const gridItems = document.querySelectorAll('.grid-item');
+
+    gridItems.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            let rgbColor = `rgb(${r}, ${g}, ${b})`;
+            item.style.backgroundColor = rgbColor;
+        });
+    });
+}
+
+const scndButton = document.querySelector(".secondButton");
+scndButton.addEventListener('click', () => {
+    ranColor();
+});
+
+
 
     
 
